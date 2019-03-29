ed4209049fbc73a5c703b86669f65585099ffc3a
Andrew Spaziani <spaziani96@gmail.com>
2019-03-29 09:50:07 -0400

    Added C# project

---
+++ C#/ACMCompetitionProblems/Problem8/Problem8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Numerics;
+using ACMHelper;
+
+namespace ACMCompetition
+{
+    class Problem8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Prompt");
+            string input = Console.ReadLine();
+        }
+    }
+}
